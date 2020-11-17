6a6cfee65ab493713be9b37e48bfcb04bfd38549
alex0blackwell <alex.blackwell314@gmail.com>
2020-11-16 18:10:22 -0800

    Update music website style, add easter egg, add new song

---
+++ pages/misc/music/js/main.js
@@ -0,0 +1,12 @@
+function toggleDateEE() {
+  var dateTitle = document.getElementById("dateTitle");
+  var date = document.getElementById("date");
+
+  if(dateTitle.innerHTML == "Date") {
+    dateTitle.innerHTML = "Dating";
+    date.innerHTML = "Annika";
+  } else {
+    dateTitle.innerHTML = "Date";
+    date.innerHTML = "November 2020";
+  }
+}
